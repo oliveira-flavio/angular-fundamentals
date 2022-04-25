56b43f71fc393d7894484b1b581cc864c01ae464
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2022-04-25 19:17:33 -0300

    Adiciona rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {path:'', redirectTo: '/home', pathMatch:'full'},
+    {path:'home', component: HomeComponent},
+    {path:'contato', component: ContatoComponent},
+    {path:'sobre', component: SobreComponent},
+    {path:'feature-data-binding', component: DataBindingComponent},
+    {path: 'produtos', component: ListaProdutoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
